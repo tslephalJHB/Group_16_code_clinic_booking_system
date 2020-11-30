5d0fe63f543cc3c92374b9f91950176359f160d9
tslephal <tslephal@student.wethinkcode.co.za>
2020-11-30 10:59:06 +0200

    Update of cancel slot

--- cancel_slot.py
+++ cancel_slot.py
@@ -9,6 +9,7 @@ import pickle
 import setup as config
 
 username = config.get_users_home_dir()
+hour,minutes = configure.get_time()
 
 CLIENT_SECRET_FILE = 'credentials.json'
 API_NAME = 'calendar'
@@ -25,29 +26,16 @@ def cancel_slot(service, eventId):
 if __name__ == "__main__":
     service = create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
     get_calendar(service)
+    
+    date = str(year)+'-'+str(month)+'-'+str(day)
+    time = str(hour)+':'+str(minutes)
+
     email = username+'@student.wethinkcode.co.za'
     event_list = open('events.csv', 'r').readlines()
-    open_slots = [event for event in event_list if 'Open Slot' in event and email in event]
-
-    open_list = []
-    count = 1
+    open_slots = [event for event in event_list if (time in event and date in event)]
 
     if len(open_slots) == 0:
-        print('No available slot for date')
+        print('No booked slots available for user')
+
     else:
-        print('Available slots:')
-    
-    for event in open_slots:
-        event = event.split(',')
-        open_list.append(event)
-        print(str(count)+'. '+event[0]+' '+event[1][:10]+' '+event[1][11:16])
-        count += 1
-
-    while True:    
-        slot = int(input('Select preferred slot: '))
-        if not is_int(slot) or slot not in range(1, len(open_list)+1):
-            print(f'Sorry, you picked an invalid slot. Please select 1 - {len(open_list)}')
-        else:
-            break
-
-    do_next = cancel_slot(service,open_list[slot - 1][3])
+        cancel_slot(service, event[3]) for event in open_slots if 'Book Slot' in event
